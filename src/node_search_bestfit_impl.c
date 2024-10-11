30ec4f318b5fe4ba6acb6bc9dc0e3250989a70a8
Ridge Tagala <s3934367@student.rmit.edu.au>
2024-10-11 13:23:50 +1100

    - implementation of bestfit

--- src/node_search_bestfit_impl.c
+++ src/node_search_bestfit_impl.c
@@ -1,5 +1,45 @@
 #include "node_search.h"
 
 AllocNode * find_and_pop_suitable_node(AllocNode **alloc_linked_list, size_t chunk_size) {
+    // initialise variables 
+    AllocNode *previous = NULL;
+    AllocNode *current = *alloc_linked_list;
+    AllocNode *best_fit = NULL;
+    AllocNode *best_fit_previous = NULL;
+
+    // iterate through linked list 
+    while (current != NULL)
+    {
+        // check current node size is >= requested size
+        if (current->chunk_size >= chunk_size)
+        {
+            // check if best fit is found
+            if (best_fit == NULL || current->chunk_size < best_fit->chunk_size)
+            {
+                best_fit = current;
+                best_fit_previous = previous;
+            }
+        }
+
+        // next node 
+        previous = current;
+        current = current->next;
+
+        // if best-fit node was found, remove from linked list 
+        if (best_fit != NULL)
+        {
+            if (best_fit_previous == NULL)
+            {
+                *alloc_linked_list = best_fit->next;
+            }
+            else
+            {
+                best_fit_previous->next = best_fit->next;
+            }
+
+            return best_fit;
+        }
+    }
+    
     return NULL;
 }
